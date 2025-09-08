c8163e3d2429cb4fda37436ce1b060c104c529b9
d.o.kononov <d.o.kononov@gmail.com>
2025-09-08 12:11:44 +0300

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Dmitry Kononov on 8.09.25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
